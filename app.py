e3d195125938a61013830f609aa8eb0b28a22295
vero-nika-2828 <>
2023-04-28 09:54:08 +0000

    Import flask

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-env.py
-__pycache__/
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World...again!"
+
+
+if __name__ == "__main__" :
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=True)
